ddf316156f8376e4869875d3c38e5ba4c1080f0d
mosidrum <mosiokanga@gmail.com>
2022-08-04 15:57:17 +0100

    update

---
+++ Adv_C/index_variadic_func.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int add(int args, ...)
+{
+	va_list ap;
+
+	va_start(ap, args);
+	
+	int i = 0, sum = 0;
+
+	for(i = 0; i < args; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return sum;
+}
+
+int main(void)
+{
+	printf("Add1 = %d\n", add(3,1,2,3));
+	printf("Add2 = %d\n", add(6,1,2,3,4,5,7));
+	return 0;
+}
